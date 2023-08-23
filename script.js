02f44aa6c2caf35d54b25ce97ead43188ccbb71f
h1ke20 <hayk.ghazaryan20@mail.ru>
2023-08-23 16:14:28 +0400

    global update 6

--- script.js
+++ script.js
@@ -71,26 +71,28 @@ $(document).ready(function () {
         }, 500);
     })
 
-    $("#phone").mask("+7 (999) 999-99-99");
+    // $("#phone").mask("+79999999999");
 
     $('.send-form').click(function (){
 
         let form = $(this).closest('form');
 
-        if (form.valid()){
-            let actUrl = form.attr('action');
-            $.ajax({
-                url: actUrl,
-                type: 'post',
-                dataType: 'html',
-                data: form.serialize(),
-                success: function (data){
-                    form.find('.status').html('Форма отправлена успешно');
-                },
-                error: function (){
-                    form.find('.status').html('Серверная ошибка');
-                }
-            });
+        console.log(formValidation())
+        if (true){
+            console.log('yuhu')
+            // let actUrl = form.attr('action');
+            // $.ajax({
+            //     url: actUrl,
+            //     type: 'post',
+            //     dataType: 'html',
+            //     data: form.serialize(),
+            //     success: function (data){
+            //         form.find('.status').html('Форма отправлена успешно');
+            //     },
+            //     error: function (){
+            //         form.find('.status').html('Серверная ошибка');
+            //     }
+            // });
         }
     });
 
@@ -127,6 +129,71 @@ $(document).ready(function () {
             window.scrollTo(0, 0);
         });
     })
+
+    function formValidation() {
+        let nameRegex = /^[a-zA-Z ]{2,30}$/;
+        let mobileRegex = /^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/;
+        let emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+        let mobile = phone.value.slice(2).split(' ').join('').split('(').join('')
+            .split(')').join('').split('-').join('');
+        let resultName = nameRegex.test(nameField.value)
+        let resultSurname = nameRegex.test(surnameField.value);
+        let resultMiddleName = nameRegex.test(middleName.value);
+        let resultEmail = emailRegex.test(emailField.value);
+        let resultMobile = mobileRegex.test(mobile);
+
+        console.log(`${mobile.length} ${mobile}`)
+        if(resultName && resultSurname
+            && (resultMiddleName || middleName.value === '') &&
+            resultEmail && resultMobile && mobile.length == 10) {
+
+            nameField.classList?.remove('fail');
+            surnameField.classList?.remove('fail');
+            middleName.classList?.remove('fail');
+            emailField.classList?.remove('fail');
+            mobile.classList?.remove('fail');
+            form_status.classList?.remove('active');
+            form_status_fail.classList?.remove('active');
+
+            let params = {
+                first_name: nameField.value,
+                last_name: surnameField.value,
+                middle_name: middleName?.value,
+                phone_number: phone.value,
+                user_email: emailField.value,
+                message: user_message.value
+            }
+
+            emailjs.send("service_dzniqqe","template_ld4nzxk", params).then(function(res) {
+                console.log('success' + typeof res.status);
+                if(res.status === 200) {
+                    form_status.classList.add('active');
+                    user_form.reset();
+                } else {
+                    form_status_fail.classList.add('active');
+                }
+            });
+
+
+        } else {
+            if(!resultName) {
+                nameField.classList.add('fail');
+            }
+            if(!resultSurname) {
+                surnameField.classList.add('fail');
+            }
+            if(!resultMiddleName) {
+                middleName.classList.add('fail');
+            }
+            if(!resultEmail) {
+                emailField.classList.add('fail');
+            }
+            if(!resultMobile || mobile.length !== 10) {
+                phone.classList.add('fail');
+            }
+        }
+
+    }
 
 });
 
